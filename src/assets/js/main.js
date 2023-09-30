5f22c109d263a711364386dc06127889f44f4138
Bakhriev <legendofpain5112@gmail.com>
2023-09-30 23:32:04 +0500

    last

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,19 @@ const scrollToBottom = element => {
 	element.scrollTo(0, messagesContainer.scrollHeight)
 }
 scrollToBottom(messagesContainer)
+
+const infoOpenBtns = document.querySelectorAll('[data-info-open-btn]')
+const infoCloseBtns = document.querySelectorAll('[data-info-close-btn]')
+
+const sectionInfo = document.querySelector('.section-info')
+infoOpenBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		sectionInfo.classList.add('active')
+	})
+})
+
+infoCloseBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		sectionInfo.classList.remove('active')
+	})
+})
